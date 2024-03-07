b713cf46903c4ae1b0e723da892bbbbf7e4df2a0
opportunityfan <3336970253@qq.com>
2024-03-07 20:11:17 +0800

    SideBar

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-
+  <div class="login-main full padding-surround">
+    <div class="login-panel center">
+      <div class="title">Sign up</div>
+      <div class="hint">Please login in to continue to your account</div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +13,16 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+.login-main
+  padding-left 20px
+  box-sizing border-box
+  position relative
 
+.login-panel
+  width 300px
+  height 600px
+  text-align left
+  div
+    margin 10px 0
 </style>
